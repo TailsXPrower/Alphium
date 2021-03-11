757214b26233930716427d266e20290ea62a0762
TailsXPrower <Dj.Delssin@gmail.com>
2021-03-11 17:37:22 +0200

    Main version of Browser

---
+++ Browser.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace ChromiumBrowserWinForms
+{
+    partial class Browser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Browser));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.ButtonGo = new System.Windows.Forms.ToolStripButton();
+            this.AddressBar = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ButtonGo,
+            this.AddressBar});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 27);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // ButtonGo
+            // 
+            this.ButtonGo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.ButtonGo.Image = ((System.Drawing.Image)(resources.GetObject("ButtonGo.Image")));
+            this.ButtonGo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ButtonGo.Name = "ButtonGo";
+            this.ButtonGo.Size = new System.Drawing.Size(32, 24);
+            this.ButtonGo.Text = "Go";
+            this.ButtonGo.Click += new System.EventHandler(this.ButtonGo_Click);
+            // 
+            // AddressBar
+            // 
+            this.AddressBar.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.AddressBar.Name = "AddressBar";
+            this.AddressBar.Size = new System.Drawing.Size(400, 27);
+            // 
+            // Browser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Browser";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Browser_FormClosing);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton ButtonGo;
+        private System.Windows.Forms.ToolStripTextBox AddressBar;
+    }
+}
+
---
+++ Browser.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace ChromiumBrowserWinForms
+{
+    public partial class Browser : Form
+    {
+        public ChromiumWebBrowser chromeBrowser = null;
+        private string initialURL = "https://datorium.eu";
+
+        public Browser()
+        {
+            InitializeComponent();
+            InitializeChromium();
+        }
+
+        public void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+            // Initialize cef with the provided settings
+            Cef.Initialize(settings);
+            // Create a browser component
+            chromeBrowser = new ChromiumWebBrowser(initialURL);
+            // Add it to the form and fill it to the form window.
+            this.Controls.Add(chromeBrowser);
+            chromeBrowser.Dock = DockStyle.Fill;
+        }
+
+        private void Browser_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+
+        private void ButtonGo_Click(object sender, EventArgs e)
+        {
+            string addressBarURL = AddressBar.Text;
+            chromeBrowser.Load(addressBarURL);
+        }
+
+        //Homework for Feb 25, 2021
+        //1. Add Back and Forward buttons, make sure that they work
+        //2. Add Reload button
+        //3. Check, if adress does not start with http/www, then request a Google search
+        //4. Make sure that when you press enter in the Address Bar, the browser vists your URL
+        //5. Your own feature
+    }
+}
